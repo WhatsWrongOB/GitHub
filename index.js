cef800ca38558062dc45b59ba509e1ce248d9d1c
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2024-05-31 23:19:29 +0500

    calling api

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
-
 const fetchData = async () => {
   try {
     const res = await fetch("https://fakestoreapi.com/products");
     const data = await res.json();
-    console.log(data)
+    console.log(data);
   } catch (error) {
     console.log(error.message);
   }
 };
-fetchData()
-
+fetchData();
